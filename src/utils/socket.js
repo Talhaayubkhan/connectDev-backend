2102c6b4fe81fbd39e6f079f5f81317017048b98
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2026-03-29 15:45:16 +0500

    add room id secure via crypto

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,27 +1,20 @@
 const socketIO = require("socket.io");
-const { CORS_OPTIONS } = require("./constants");
+const { CORS_OPTIONS, generateChatRoomId } = require("./constants");
 
 const initSocket = (server) => {
   const io = socketIO(server, { cors: CORS_OPTIONS });
 
   io.on("connection", (clientSocket) => {
     clientSocket.on("joinChat", ({ firstName, IdUser, userId }) => {
-      const currentUserId = IdUser;
-      const chatPartnerId = userId;
-
-      const chatRoomId = [currentUserId, chatPartnerId].sort().join("_");
-
-      console.log(`${firstName} room joining ${chatRoomId}`);
-
-      clientSocket.join(chatRoomId);
+      const roomId = generateChatRoomId(IdUser, userId);
+      clientSocket.join(roomId);
     });
 
     clientSocket.on("sendMessage", ({ firstName, IdUser, userId, text }) => {
-      const currentUserId = IdUser;
-      const chatPartnerId = userId;
-      const roomId = [currentUserId, chatPartnerId].sort().join("_");
-      console.log(`${firstName} has recevied the ${text}`);
-      io.to(roomId).emit("messageRecevied", { firstName, text });
+      const roomId = generateChatRoomId(IdUser, userId);
+
+      // ✅ FIXED HERE
+      io.to(roomId).emit("messageReceived", { firstName, text });
     });
   });
 
